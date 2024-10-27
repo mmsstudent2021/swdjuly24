5afa1b65898e934ff82ac202b3798c83645f4907
mmsstudent2021 <mmsstudent2021@gmail.com>
2024-10-27 14:32:03 +0630

    way point

--- 9-JSLibs/js-libs/main.js
+++ 9-JSLibs/js-libs/main.js
@@ -1,1 +1,23 @@
 import './style.css'
+import './node_modules/waypoints/lib/noframework.waypoints'
+
+const waypointDiv = document.getElementById("waypoint_div");
+let button = document.getElementById("waypoint_button")
+const testButton = new Waypoint({
+  element : document.getElementById('waypoint_div'),
+  handler : function (direction){
+    if (direction === 'down'){
+     waypointDiv.textContent="Down"
+     button.classList.toggle("opacity-0")
+
+      
+    }
+    else{
+      console.log("up")
+      waypointDiv.textContent="Up"
+      button.classList.toggle("opacity-0")
+      
+    }
+  },
+  offset:'15%'
+})
